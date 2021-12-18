0dd96588c3037bbb4fae05ac8622e3f74a12b89e
ramyabdalghafour <96006648+ramyabdalghafour@users.noreply.github.com>
2021-12-18 18:32:20 +0200

    Add files via upload

---
+++ src/lec30/Lec30.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lec30;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author esaid
+ */
+public class Lec30 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+  
+for(int x = 2 ;x<=60 ;x+=2){
+    
+    if(20<x&&x<40){
+         continue;}
+    System.out.println(x);
+     
+ }
+    }
+    
+}
